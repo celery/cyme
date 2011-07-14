a5c17eca89cb9b78759b0b6c05639bbaf232e46d
Ask Solem <ask@rabbitmq.com>
2011-07-14 13:14:08 +0100

    Cosmetics

--- scs/models.py
+++ scs/models.py
@@ -32,6 +32,8 @@ class Queue(models.Model):
                                    default=None, null=True, blank=True)
     is_enabled = models.BooleanField(_(u"is enabled"), default=True)
     created_at = models.DateTimeField(_(u"created at"), auto_now_add=True)
+
+    #: Additional JSON encoded queue options.
     options = models.TextField(null=True, blank=True)
 
     class Meta:
@@ -44,6 +46,7 @@ class Queue(models.Model):
 
 class Node(models.Model):
     Queue = Queue
+
     objects = NodeManager()
     mutex = Lock()
     cwd = CWD
@@ -71,21 +74,28 @@ class Node(models.Model):
         self.save()
 
     def start(self, **kwargs):
+        """Starts the instance."""
         return self._action("start", **kwargs)
 
     def stop(self, **kwargs):
+        """Shuts down the instance."""
         return self._action("stop", **kwargs)
 
     def restart(self, **kwargs):
+        """Restarts the instance."""
         return self._action("restart", **kwargs)
 
     def alive(self, timeout=1):
+        """Returns :const:`True` if the pid responds to signals,
+        and the instance responds to ping broadcasts."""
         return self.responds_to_signal() and self.responds_to_ping(timeout)
 
     def stats(self):
+        """Returns node statistics (as ``celeryctl inspect stats``)."""
         return self._query("stats")
 
     def autoscale(self, max=None, min=None):
+        """Set min/max autoscale settings."""
         if max is not None:
             self.max_concurrency = max
         if min is not None:
@@ -93,16 +103,14 @@ class Node(models.Model):
         self.save()
         return self._query("autoscale", max=max, min=min)
 
-    def _action(self, action, multi="celeryd-multi"):
-        with self.mutex:
-            argv = ([multi, action, '--suffix=""', "--no-color", self.name]
-                  + list(self.argv) + list(self.default_args))
-            return self.multi.execute_from_commandline(argv)
-
     def responds_to_ping(self, timeout=1):
+        """Returns :const:`True` if the instance responds to
+        broadcast ping."""
         return True if self._query("ping", timeout=timeout) else False
 
     def responds_to_signal(self):
+        """Returns :const:`True` if the pidfile exists and the pid
+        responds to signals."""
         pid = self.getpid()
         if not pid:
             return False
@@ -114,23 +122,15 @@ class Node(models.Model):
             raise
         return True
 
-    def _query(self, cmd, **kwargs):
-        name = self.name
-        timeout = kwargs.pop("timeout", None) or 1
-        r = celery.control.broadcast(cmd, arguments=kwargs,
-                   destination=[name], reply=True, timeout=timeout)
-        if r:
-            for reply in r:
-                if name in reply:
-                    return reply[name]
-
     def consuming_from(self):
+        """Return the queues the instance is currently consuming from."""
         queues = self._query("active_queues")
         if queues:
             return dict((q["name"], q) for q in queues)
         return {}
 
     def add_queue(self, name):
+        """Add queue for this instance by name."""
         q = self.queues.get(name=name)
         options = deserialize(q.options) if q.options else {}
         exchange = q.exchange if q.exchange is not None else name
@@ -142,16 +142,36 @@ class Node(models.Model):
                                     **options)
 
     def cancel_queue(self, queue):
+        """Cancel queue for this instance by :class:`Queue`."""
         self._query("cancel_consumer", queue=queue.name)
 
     def getpid(self):
+        """Get the process id for this instance by reading the pidfile.
+
+        Returns :const:`None` if the pidfile does not exist.
+
+        """
         pidfile = self.pidfile.replace("%n", self.name)
         return platforms.PIDFile(pidfile).read_pid()
 
-    @property
-    def strargv(self):
-        return " ".join("%s %s" % (k, shellquote(str(v)))
-                            for k, v in self.argtuple if v)
+
+    def _action(self, action, multi="celeryd-multi"):
+        """Execute :program:`celeryd-multi` command."""
+        with self.mutex:
+            argv = ([multi, action, '--suffix=""', "--no-color", self.name]
+                  + list(self.argv) + list(self.default_args))
+            return self.multi.execute_from_commandline(argv)
+
+    def _query(self, cmd, **kwargs):
+        """Send remote control command and wait for this instances reply."""
+        name = self.name
+        timeout = kwargs.pop("timeout", None) or 1
+        r = celery.control.broadcast(cmd, arguments=kwargs,
+                   destination=[name], reply=True, timeout=timeout)
+        if r:
+            for reply in r:
+                if name in reply:
+                    return reply[name]
 
     @property
     def argv(self):
