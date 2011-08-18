067156c3b8782579bb51884e4a5a36f950e020d8
Ask Solem <ask@rabbitmq.com>
2011-08-18 17:41:15 +0100

    Improves the Client API

---
+++ scs/client/__init__.py
@@ -0,0 +1,110 @@
+"""scs.client"""
+
+from __future__ import absolute_import
+
+from . import base
+
+class Client(base.Client):
+    app = "scs"
+
+    class Instances(base.Section):
+
+        class Model(base.Model):
+            repr = "Instance"
+            fields = ("name", "broker", "min_concurrency", "max_concurrency",
+                      "queues", "is_enabled")
+
+            class Consumers(base.Section):
+
+                def __init__(self, client, name):
+                    base.Section.__init__(self, client)
+                    self.path = self.client.path / name / "queues"
+
+                def add(self, queue):
+                    return self.PUT(self.path / queue)
+
+                def remove(self, queue):
+                    return self.DELETE(self.path / queue)
+
+                def all(self):
+                    return self.GET(self.path)
+
+            def __init__(self, *args, **kwargs):
+                base.Model.__init__(self, *args, **kwargs)
+                self.consumers = self.Consumers(self.parent, name=self.name)
+                self.path = self.parent.path / self.name
+
+            def stats(self):
+                return self.parent.stats(self.name)
+
+            def autoscale(self, max=None, min=None):
+                return self.parent.autoscale(self.name, max=max, min=min)
+
+        def add(self, name=None):
+            # name is optional for instances
+            return base.Section.add(self, name)
+
+        def stats(self, name):
+            return self.GET(self.path / name / "stats")
+
+        def autoscale(self, name, max=None, min=None):
+            return self.POST(self.path / name / "autoscale",
+                             params={"max": max, "min": min})
+
+    class Queues(base.Section):
+        repr = "Queue"
+        fields = ("name", "exchange", "exchange_type",
+                  "routing_key", "options")
+
+        def delete(self, name):
+            return self.DELETE(self.path / name)
+
+        def add(self, name, exchange=None, exchange_type=None,
+                routing_key=None, **options):
+            options = anyjson.serialize(options) if options else None
+            return self.PUT(self.path / name,
+                            data={"exchange": exchange,
+                                "exchange_type": exchange_type,
+                                "routing_key": routing_key,
+                                "options": options}, type=self.create_model)
+
+    def __init__(self, url=None, app=None, info=None):
+        super(Client, self).__init__(url)
+        self.app = app
+        self.instances = self.Instances(self)
+        self.queues = self.Queues(self)
+        self.info = info or {}
+
+    def add(self, name, hostname=None, port=None, userid=None,
+            password=None, virtual_host=None):
+        return self.create_model(name, self.root("PUT", name,
+                                 data={"hostname": hostname,
+                                       "port": port,
+                                        "userid": userid,
+                                        "password": password,
+                                        "virtual_host": virtual_host}))
+
+    def get(self, name=None):
+        return self.create_model(name, self.root("GET", name or self.app))
+
+    def delete(self, name=None):
+        return self.root("DELETE", name or self.app)
+
+    def all(self):
+        return self.root("GET")
+
+    def build_url(self, path):
+        url = self.url
+        if self.app:
+            url += "/" + self.app
+        return url + str(path)
+
+    def create_model(self, name, info):
+        return self.clone(app=name, info=base.AttributeDict(info))
+
+    def clone(self, app=None, info=None):
+        return self.__class__(url=self.url, app=app, info=info)
+
+    def __repr__(self):
+        return "<Client: %r>" % (self.build_url(""), )
+
---
+++ scs/client/base.py
@@ -0,0 +1,149 @@
+"""scs.client.base"""
+
+from __future__ import absolute_import
+
+import anyjson
+import requests
+
+from urllib import quote
+
+from celery.datastructures import AttributeDict
+
+from .. import __version__
+from ..utils import cached_property
+
+
+class Path(object):
+
+    def __init__(self, s=None, stack=None):
+        self.stack = (stack or []) + (s.stack if isinstance(s, Path) else [s])
+
+    def __str__(self, s="/"):
+        return (s + s.join(map(quote, filter(None, self.stack))).strip(s) + s)
+
+    def __div__(self, other):
+        return Path(other, self.stack)
+
+
+class Base(object):
+
+    def serialize(self, obj):
+        return anyjson.serialize(obj)
+
+    def deserialize(self, text):
+        return anyjson.deserialize(text)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __delitem__(self, key):
+        return self.delete(key)
+
+    def keys(self):
+        return self.all()
+
+    def __iter__(self):
+        return iter(self.keys())
+
+
+class Model(object):
+    repr = "Model"
+    repr_field = "name"
+    fields = ()
+
+    def __init__(self, parent, *args, **kwargs):
+        self.parent = parent
+        if len(args) == 1 and isinstance(args[0], dict):
+            kwargs.update(args[0])
+
+        for field in self.fields:
+            setattr(self, field, None)
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
+    def delete(self):
+        return self.parent.delete(self.name)
+
+    def __repr__(self):
+        return "<%s: %r>" % (self.repr,
+                             getattr(self, self.repr_field, None) or "(none)")
+
+
+class Section(Base):
+    Model = Model
+    name = None
+    path = None
+    proxy = ["GET", "POST", "PUT", "DELETE"]
+
+    def __init__(self, client):
+        self.client = client
+        if self.name is None:
+            self.name = self.__class__.__name__.lower()
+        for attr in self.proxy:
+            setattr(self, attr, getattr(self.client, attr))
+        self.path = Path(self.name) if self.path is None else self.path
+
+    def all(self):
+        return self.GET(self.path)
+
+    def get(self, name):
+        return self.GET(self.path / name, type=self.create_model)
+
+    def add(self, name, **data):
+        return self.PUT(self.path / name, type=self.create_model, data=data)
+
+    def delete(self, name):
+        return self.DELETE(self.path / name)
+
+    def create_model(self, *args, **kwargs):
+        return self.Model(self, *args, **kwargs)
+
+    def __repr__(self):
+        return repr(self.all())
+
+
+class Client(Base):
+    default_url = "http://localhost:8000"
+
+    def __init__(self, url=None):
+        self.url = url.rstrip("/") if url else self.default_url
+
+    def GET(self, path, params=None, type=None):
+        return self.request("GET", path, params, None, type)
+
+    def PUT(self, path, params=None, data=None, type=None):
+        return self.request("PUT", path, params, data, type)
+
+    def POST(self, path, params=None, data=None, type=None):
+        return self.request("POST", path, params, data, type)
+
+    def DELETE(self, path, params=None, data=None, type=None):
+        return self.request("DELETE", path, params, data, type)
+
+    def request(self, method, path, params=None, data=None, type=None):
+        return self._request(method, self.build_url(path), params, data, type)
+
+    def _request(self, method, url, params=None, data=None, type=None):
+        type = type or AttributeDict
+        r = requests.request(method, str(url),
+                             headers=self.headers,
+                             params=params, data=data)
+        if r.ok:
+            ret = self.deserialize(r.read())
+            if isinstance(ret, dict):
+                return type(ret)
+            return ret
+        r.raise_for_status()
+
+    def root(self, method, path=None, params=None, data=None):
+        return self._request(method,
+                             self.url + str(Path(path) if path else ""),
+                             params, data)
+
+    def __repr__(self):
+        return "<Client: %r>" % (self.url, )
+
+    @cached_property
+    def headers(self):
+        return {"Accept": "application/json",
+                "User-Agent": "scs-client %r" % (__version__, )}
